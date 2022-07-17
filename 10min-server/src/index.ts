197d7f90b010901c21e597c7abf2f0a2933e569c
Povilas Tamosauskas <povilast@hotmail.lt>
2022-07-17 10:39:02 +0300

    adding commons

--- 10min-server/src/index.ts
+++ 10min-server/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { PORT } from './config/constants';
 import { articleRouter } from './routes';
+import { CheckIsFoo } from '10min-commons';
 
 const app = express();
 app.use(express.json());
 
 app.use('/article', articleRouter)
+
+console.log({CheckIsFoo: CheckIsFoo("foo")})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
